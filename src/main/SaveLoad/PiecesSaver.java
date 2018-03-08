2f557e790570618f81175fb0e9214d4cdcab22ec
Amy Hong <amyhong0502@gmail.com>
2018-03-08 12:20:20 -0800

    added javadoc

--- src/main/SaveLoad/PiecesSaver.java
+++ src/main/SaveLoad/PiecesSaver.java
@@ -2,14 +2,41 @@ package main.SaveLoad;
 
 import java.io.Serializable;
 
+/**
+ * Saves one piece per each PieceSaver object.
+ * @author Amy
+ */
 public class PiecesSaver implements Serializable {
+    
+    /** Column index of this piece to save. */
     private final int columnIndex;
+    
+    /** Row index of this piece to save. */
     private final int rowIndex;
+    
+    /**
+     * Type of this piece to save: 
+     * Pawn, Knight, Rook, Bishop, Queen, or King. 
+     */
     private final char type;
+    
+    /** True if this piece to save is white. */
     private final boolean white;
+    
+    /** True if this piece to save has never moved. */
     private final boolean firstMove;
-
-    PiecesSaver(boolean white, char type, int columnIndex, int rowIndex, boolean firstMove) {
+    
+    /**
+     * Constructor of this PieceSaver.
+     * @param white       True if this piece to save is white
+     * @param type        Type of this piece to save: 
+     *                    Pawn, Knight, Rook, Bishop, Queen, or King
+     * @param columnIndex Column index of this piece to save
+     * @param rowIndex    Row index of this piece to save
+     * @param firstMove   True if this piece to save has never moved
+     */ 
+    PiecesSaver(boolean white, char type,
+                int columnIndex, int rowIndex, boolean firstMove) {
         this.white = white;
         this.type = type;
         this.columnIndex = columnIndex;
@@ -17,21 +44,45 @@ public class PiecesSaver implements Serializable {
         this.firstMove = firstMove;
     }
 
+    /**
+     * Returns type of this saved piece.
+     * @return type of this saved piece
+     */
     public char getType() {
         return type;
     }
-
+    
+    /**
+     * Returns true if this saved piece is white. 
+     * @return true if this saved piece is white
+     */
     public boolean isWhite() {
         return white;
     }
 
+    /**
+     * Returns column's index of this saved piece.
+     * @return column's index of this saved piece
+     */
     public int getColumnIndex() {
         return columnIndex;
     }
 
+    /**
+     * Returns row's index of this saved piece.
+     * @return row's index of this saved piece
+     */
     public int getRowIndex() {
         return rowIndex;
     }
 
-    public boolean isFirstMove() { return firstMove; }
+    /**
+     * Returns true if this saved piece never moved from the beginning of 
+     * this saved game.
+     * @return true if this saved piece never moved 
+     *              from the beginning of this saved game
+     */
+    public boolean isFirstMove() {
+        return firstMove; 
+    }
 }
